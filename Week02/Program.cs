fa5d2570abb0873329b9c8107dec53c18431361a
Safal <bhattarairobert@gmail.com>
2025-11-18 20:33:42 +0545

    Week02 file added

---
+++ Week02/Program.cs
@@ -0,0 +1,8 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string quote = "If you can't yet do great things, do small things in a great way.";
+        Console.WriteLine(quote);
+    }
+}
