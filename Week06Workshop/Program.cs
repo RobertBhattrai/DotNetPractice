7964fd99f69eeb189a6f8f4e4d8d89d8c84c5231
Safal <bhattarairobert@gmail.com>
2025-12-02 10:40:12 +0545

    Folder Created for Week 06

---
+++ Week06Workshop/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Week06Workshop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
