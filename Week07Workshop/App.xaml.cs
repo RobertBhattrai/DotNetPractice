2e8ab279386423e9a98298173b326beddba467ec
Safal <bhattarairobert@gmail.com>
2025-12-12 19:40:22 +0545

    Project Setup

---
+++ Week07Workshop/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Week07Workshop
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Week07Workshop/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Week07Workshop
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
